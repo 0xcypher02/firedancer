9c3f819497db0c7dd2f03cfa5070a7927d628602
Kunal Bhargava <kbhargava@jumptrading.com>
2024-06-06 06:16:48 +0000

    contrib: add rust script for ledgers

---
+++ contrib/create-ledgers/src/main.rs
@@ -0,0 +1,215 @@
+use {
+    solana_sdk::{
+        // bpf_loader,
+        bpf_loader_upgradeable,
+        bpf_loader_upgradeable::{
+            UpgradeableLoaderState,
+        },
+        // loader_upgradeable_instruction::UpgradeableLoaderInstruction,
+        pubkey::{
+            Pubkey
+        },
+        account::{
+            AccountSharedData,
+            WritableAccount,
+            ReadableAccount
+        },
+        instruction::{
+            AccountMeta,
+            InstructionError
+        },
+        signature::{Keypair, read_keypair_file, Signer},
+        system_instruction,
+        transaction::Transaction,
+        transaction_context::{IndexOfAccount},
+        rent::Rent,
+        commitment_config::CommitmentConfig,
+        feature_set::FeatureSet,
+        message::Message,
+    },
+    solana_client::{
+        rpc_client::RpcClient,
+    },
+    solana_cli::{
+        program::calculate_max_chunk_size,
+    },
+    solana_bpf_loader_program::{
+        self,
+        test_utils,
+        syscalls::create_program_runtime_environment_v1
+    },
+    solana_program_runtime::{
+        invoke_context::{InvokeContext},
+        compute_budget::ComputeBudget
+    },
+    solana_rbpf::{
+        declare_builtin_function,
+        memory_region::{MemoryMapping},
+        elf::Executable,
+        verifier::RequisiteVerifier
+    },
+    std::{fs::File, io::Read, error::Error, sync::Arc},
+};
+
+
+fn load_program_account_from_elf(loader_id: &Pubkey, path: &str) -> AccountSharedData {
+    let mut file = File::open(path).expect("file open failed");
+    let mut elf = Vec::new();
+    file.read_to_end(&mut elf).unwrap();
+    let rent = Rent::default();
+    let mut program_account =
+        AccountSharedData::new(rent.minimum_balance(elf.len()), 0, loader_id);
+    program_account.set_data(elf);
+    program_account.set_executable(true);
+    program_account
+}
+
+fn read_and_verify_elf(program_location: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let mut file = File::open(program_location)
+        .map_err(|err| format!("Unable to open program file: {err}"))?;
+    let mut program_data = Vec::new();
+    file.read_to_end(&mut program_data)
+        .map_err(|err| format!("Unable to read program file: {err}"))?;
+
+    // Verify the program
+    let program_runtime_environment = create_program_runtime_environment_v1(
+        &FeatureSet::all_enabled(),
+        &ComputeBudget::default(),
+        true,
+        false,
+    )
+    .unwrap();
+    let executable =
+        Executable::<InvokeContext>::from_elf(&program_data, Arc::new(program_runtime_environment))
+            .map_err(|err| format!("ELF error: {err}"))?;
+
+    executable
+        .verify::<RequisiteVerifier>()
+        .map_err(|err| format!("ELF error: {err}"))?;
+
+    Ok(program_data)
+}
+
+
+fn main() {
+    let validator_url = "http://localhost:8899";
+    let client = RpcClient::new_with_commitment(validator_url, CommitmentConfig::confirmed());
+    let loader_id = bpf_loader_upgradeable::id();
+
+    let program_path = "/home/kbhargava/repos/sbf/helloworld.so";
+    let program_keypair_path = "/home/kbhargava/repos/sbf/helloworld-keypair.json";
+
+    match client.get_slot() {
+        Ok(slot) => println!("Current slot: {}", slot),
+        Err(e) => eprintln!("Error retrieving slot: {}", e),
+    }
+    
+    let payer_keypair_path = "/home/kbhargava/.config/solana/id.json";
+    let payer = read_keypair_file(payer_keypair_path).unwrap();
+
+    let run_account = Keypair::new();
+    let program_account = load_program_account_from_elf(&loader_id, program_path);
+    
+    let program_keypair = read_keypair_file(program_keypair_path).unwrap();
+
+    let program_data = read_and_verify_elf(program_path).unwrap();
+    let min_rent_exempt_program_data_balance = client.get_minimum_balance_for_rent_exemption(
+        UpgradeableLoaderState::size_of_programdata(program_data.len()),
+    ).unwrap(); 
+
+    let create_program_account_instruction = bpf_loader_upgradeable::create_buffer(
+        &payer.pubkey(),
+        &run_account.pubkey(),
+        &payer.pubkey(),
+        min_rent_exempt_program_data_balance,
+        program_data.len(),
+    );
+
+    let buffer_transaction = Transaction::new_signed_with_payer(
+        &create_program_account_instruction.unwrap(),
+        Some(&payer.pubkey()),
+        &[&payer, &run_account],
+        client.get_latest_blockhash().unwrap(),
+    );
+
+    let result = client.send_and_confirm_transaction(&buffer_transaction);
+    println!("New account created with pubkey: {}", run_account.pubkey());
+    println!("Transaction signature: {}", result.unwrap());
+
+    let blockhash = client.get_latest_blockhash().unwrap();
+
+    let create_msg = |offset: u32, bytes: Vec<u8>| {
+        let write_instruction = bpf_loader_upgradeable::write(
+            &run_account.pubkey(),
+            &payer.pubkey(),
+            offset,
+            bytes,
+        );
+        return Message::new_with_blockhash(&[write_instruction], Some(&payer.pubkey()), &blockhash);
+    };
+
+    
+    let chunk_size = calculate_max_chunk_size(&create_msg);
+    println!("Chunk size: {}", chunk_size);
+    // let chunks = program_data.chunks(chunk_size);
+    
+    let mut offset = 0;
+
+    for (chunk, i) in program_data.chunks(chunk_size).zip(0..) {
+        let message = create_msg((i * chunk_size) as u32, chunk.to_vec());
+
+        let mut write_transaction = Transaction::new_unsigned(
+            message,
+        );
+        write_transaction.try_sign(&[&payer], blockhash);
+    
+        let write_result = client.send_and_confirm_transaction(&write_transaction);
+        offset += chunk.len() as u32;
+        println!("{} bytes written", offset);
+    }
+
+    let mut final_instructions = bpf_loader_upgradeable::upgrade(
+        &program_keypair.pubkey(),
+        &run_account.pubkey(),
+        &payer.pubkey(),
+        &payer.pubkey(),
+    );
+
+    let final_message = Message::new_with_blockhash(
+        &[final_instructions],
+        Some(&payer.pubkey()),
+        &blockhash,
+    );
+
+    let create_executable_instruction = system_instruction::create_account(
+        &payer.pubkey(),
+        &program_keypair.pubkey(),
+        min_rent_exempt_program_data_balance,
+        program_data.len() as u64,
+        &loader_id,
+    );
+
+    let initialize_executable_instruction = system_instruction::assign(
+        &program_keypair.pubkey(),
+        &loader_id,
+    );
+
+    let mut executable_transaction = Transaction::new_with_payer(
+        &[create_executable_instruction, initialize_executable_instruction],
+        Some(&payer.pubkey()),
+    );
+
+    executable_transaction.sign(&[&payer, &program_keypair], blockhash);
+    let result = client.send_and_confirm_transaction(&executable_transaction);
+    println!("Created Account: {}", program_keypair.pubkey());
+
+
+    let mut final_tx = Transaction::new_unsigned(final_message.clone());
+    let blockhash = client.get_latest_blockhash().unwrap();
+    let mut signers: Vec<&dyn Signer> = vec![&payer];
+    let result = final_tx.try_sign(&signers, blockhash);
+    let final_result = client.send_and_confirm_transaction(&final_tx);
+
+    println!("Final program deployed: {}", program_keypair.pubkey());
+    println!("Transaction signature: {}", final_result.unwrap());
+}
